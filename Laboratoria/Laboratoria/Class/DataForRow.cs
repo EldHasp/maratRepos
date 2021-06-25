48c11c729a0526c6c2163abe77a2257f7705e872
Хасп Элд <EldHasp@Gmail.com>
2021-06-25 14:27:20 +0300

    Добавлен проект общей библиотеки

--- Laboratoria/Laboratoria/Class/DataForRow.cs
+++ Laboratoria/Laboratoria/Class/DataForRow.cs
@@ -1,34 +1,19 @@
-﻿using Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Simplified;
 
 namespace Laboratoria.Class
 {
-    public class DataForRow : OnPropertyChangedClass
+    public class DataForRow : BaseInpc
     {
         private string _symbol;
         private decimal _price;
         private ValueDirection _direction;
 
         public int ID { get; }
-        public string Symbol { get => _symbol; set => SetProperty(ref _symbol, value); }
-        public decimal Price { get => _price; set => SetProperty(ref _price, value); }
-        public ValueDirection Direction { get => _direction; set => SetProperty(ref _direction, value); }
+        public string Symbol { get => _symbol; set => Set(ref _symbol, value); }
+        public decimal Price { get => _price; set => Set(ref _price, value); }
+        public ValueDirection Direction { get => _direction; set => Set(ref _direction, value); }
 
         public DataForRow(int id) => ID = id;
 
-        protected override void SetProperty<T>(ref T fieldProperty, T newValue, [CallerMemberName] string propertyName = "")
-        {
-            if (propertyName == nameof(Price) && newValue is IComparable numb)
-            {
-                Direction = (ValueDirection)numb.CompareTo(Price);
-            }
-            base.SetProperty(ref fieldProperty, newValue, propertyName);
-
-        }
     }
 }
